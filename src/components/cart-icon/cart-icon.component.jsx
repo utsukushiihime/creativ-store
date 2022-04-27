469a50d701f204e2819173d106dbad9b72624205
Crystal McNeil <crystalsmcneil@gmail.com>
2022-04-26 23:56:43 -0600

    updates cart item icon and context creates checkout route

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -3,26 +3,18 @@ import { useContext } from 'react';
 import './cart-icon.styles.scss';
 import { CartContext } from '../../contexts/cart.context';
 
+
 const CartIcon = () => {
-	// get cart items from cart context
-	const { cartItems } = useContext(CartContext);
+  const { isCartOpen, setIsCartOpen, cartCount } = useContext(CartContext);
 
-	// if cart items is empty, return 0
-	// if cart item is in cart return the quantity of items
-	const itemCount = cartItems.reduce((accumulatedQuantity, cartItem) => {
-		return accumulatedQuantity + cartItem.quantity;
-	}, 0);
+  const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
 
-	const { isCartOpen, setIsCartOpen } = useContext(CartContext);
-
-	const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
-
-	return (
-		<div className='cart-icon-container' onClick={toggleIsCartOpen}>
-			<ShoppingIcon className='shopping-icon' />
-			<span className='item-count'>{itemCount}</span>
-		</div>
-	);
+  return (
+    <div className='cart-icon-container' onClick={toggleIsCartOpen}>
+      <ShoppingIcon className='shopping-icon' />
+      <span className='item-count'>{cartCount}</span>
+    </div>
+  );
 };
 
 export default CartIcon;
