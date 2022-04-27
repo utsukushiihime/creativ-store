720352051ae1914a4df717632fdd4c13dcb3661f
Crystal McNeil <crystalsmcneil@gmail.com>
2022-04-27 02:38:52 -0600

    adds checkout icons

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -3,6 +3,11 @@ import { useContext } from 'react';
 import { CartContext } from '../../contexts/cart.context';
 
 import './checkout-item.styles.scss';
+import {
+	AiFillMinusCircle,
+	AiFillPlusCircle,
+	AiFillDelete,
+} from 'react-icons/ai';
 
 const CheckoutItem = ({ cartItem }) => {
 	const { name, imageUrl, price, quantity } = cartItem;
@@ -22,16 +27,16 @@ const CheckoutItem = ({ cartItem }) => {
 			<span className='name'> {name} </span>
 			<span className='quantity'>
 				<div className='arrow' onClick={removeItemHandler}>
-					&#10094;
+					<AiFillMinusCircle />
 				</div>
 				<span className='value'>{quantity}</span>
 				<div className='arrow' onClick={addItemHandler}>
-					&#10095;
+					<AiFillPlusCircle />
 				</div>
 			</span>
 			<span className='price'> {price}</span>
 			<div className='remove-button' onClick={clearItemHandler}>
-				&#10005;
+				<AiFillDelete />
 			</div>
 		</div>
 	);
