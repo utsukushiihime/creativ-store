cc5ba52c366b177edd0e4bcfdf7f87f226be815a
Crystal McNeil <crystalsmcneil@gmail.com>
2022-04-28 15:57:35 -0600

    maps products and categories to shop from Firebase

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,15 +1,27 @@
-import { useContext } from 'react';
-import { ProductsContext } from '../../contexts/products.context';
+import { useContext, Fragment } from 'react';
+import { CategoriesContext } from '../../contexts/categories.context';
 import ProductCard from '../../components/product-card/product-card.component';
 import { Container, Row } from 'react-bootstrap';
+import './shop.styles.scss';
 
 const Shop = () => {
-	const { products } = useContext(ProductsContext);
+	const { categoriesMap } = useContext(CategoriesContext);
 	return (
 		<Container className='mt-4'>
+			<h1> SHOPPE</h1>
 			<Row>
-				{products.map((product) => (
-					<ProductCard key={product.id} product={product} />
+				{Object.keys(categoriesMap).map((title) => (
+					<Fragment key={title}>
+						<h2>{title}</h2>
+						<div className='row'>
+							{categoriesMap[title].map((product) => (
+								<ProductCard
+									key={product.id}
+									product={product}
+								/>
+							))}
+						</div>
+					</Fragment>
 				))}
 			</Row>
 		</Container>
