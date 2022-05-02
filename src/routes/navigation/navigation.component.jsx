5109ad93dcfcbbdb92b25b8f9458c655907b84b9
Crystal McNeil <crystalsmcneil@gmail.com>
2022-05-01 18:14:49 -0600

    adds styled components to navigation

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,89 +1,48 @@
-import { useContext } from 'react';
-import { Outlet } from 'react-router-dom';
-import { Navbar, Container, NavDropdown, Nav } from 'react-bootstrap';
+import { Fragment, useContext } from 'react';
+import { Outlet, Link } from 'react-router-dom';
 
-import { ReactComponent as Logo } from '../../assets/images/logos/creativarian-logo.svg';
+import CartIcon from '../../components/cart-icon/cart-icon.component';
+import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
+
 import { UserContext } from '../../contexts/user.context';
 import { CartContext } from '../../contexts/cart.context';
+
+import { ReactComponent as Logo } from '../../assets/images/logos/creativarian-logo.svg';
 import { signOutUser } from '../../utils/firebase/firebase.utils';
-import CartIcon from '../../components/cart-icon/cart-icon.component';
-import CartDropDown from '../../components/cart-dropdown/cart-dropdown.component';
+
+import {
+	NavigationContainer,
+	NavLinks,
+	NavLink,
+	LogoContainer,
+} from './navigation.styles';
 
 const Navigation = () => {
 	const { currentUser } = useContext(UserContext);
 	const { isCartOpen } = useContext(CartContext);
 
-	const signOutHandler = async () => {
-		await signOutUser();
-
-		console.log('signed out');
-	};
-
 	return (
-		<>
-			<Navbar
-				collapseOnSelect
-				expand='lg'
-				bg='dark'
-				variant='dark'
-				fixed='top'
-			>
-				<Container fluid>
-					<Navbar.Brand href='/'>
-						<Logo style={{ width: '50px' }} className='logo' />
-					</Navbar.Brand>
-					<Navbar.Toggle aria-controls='navbarScroll' />
-					<Navbar.Collapse
-						id='navbarScroll'
-						className='justify-content-end'
-					>
-						<Nav
-							className='me-auto my-2 my-lg-0'
-							style={{ maxHeight: '100px' }}
-							navbarScroll
-						>
-							<Nav.Link href='/'>Home</Nav.Link>
-							<NavDropdown
-								title='Services'
-								id='navbarScrollingDropdown'
-							>
-								<NavDropdown.Item href='/services/development'>
-									Development
-								</NavDropdown.Item>
-								<NavDropdown.Item href='/services/design'>
-									Design
-								</NavDropdown.Item>
-								<NavDropdown.Item href='/services/seo'>
-									SEO
-								</NavDropdown.Item>
-								<NavDropdown.Item href='/services/content-writing'>
-									Content Writing
-								</NavDropdown.Item>
-								<NavDropdown.Item href='/services/consulting'>
-									Consulting
-								</NavDropdown.Item>
-							</NavDropdown>
-						</Nav>
-						<Nav>
-							<Nav.Link href='/shop'>Shop</Nav.Link>
-							{currentUser ? (
-								<Nav.Link>
-									<span onClick={signOutHandler}>
-										Sign Out
-									</span>
-								</Nav.Link>
-							) : (
-								<Nav.Link href='/auth'>Sign In</Nav.Link>
-							)}
-						</Nav>
-						<CartIcon />
-						{isCartOpen && <CartDropDown />}
-					</Navbar.Collapse>
-				</Container>
-			</Navbar>
+		<Fragment>
+			<NavigationContainer>
+				<LogoContainer to='/'>
+					<Logo className='logo' />
+				</LogoContainer>
+				<NavLinks>
+					<NavLink to='/shop'>SHOP</NavLink>
 
+					{currentUser ? (
+						<NavLink as='span' onClick={signOutUser}>
+							SIGN OUT
+						</NavLink>
+					) : (
+						<NavLink to='/auth'>SIGN IN</NavLink>
+					)}
+					<CartIcon />
+				</NavLinks>
+				{isCartOpen && <CartDropdown />}
+			</NavigationContainer>
 			<Outlet />
-		</>
+		</Fragment>
 	);
 };
 
